19c6a4b9192d7668d4b8972d5b1fedd33edab320
Git-zaki <AbdouBox@AbdouBox-PC>
2014-12-07 15:05:29 +0100

    One to one

--- Desktop/Supsms1/src/java/supsms/dao/entity/SupBill.java
+++ Desktop/Supsms1/src/java/supsms/dao/entity/SupBill.java
@@ -1,15 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package supsms.dao.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -18,41 +18,47 @@ import javax.persistence.Id;
 @Entity
 public class SupBill implements Serializable {
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column (name = "id_Bill")
+    private Long Id_Bill;
 
-    public Long getId() {
-        return id;
+    @Column (name = "bill_date", nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date Bill_date;
+    
+    @OneToOne
+    @JoinColumn(name="id_Subscription")
+    private SupPayment supPayment;
+
+    public Long getId_Bill() {
+        return Id_Bill;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId_Bill(Long Id_Bill) {
+        this.Id_Bill = Id_Bill;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public Date getBill_date() {
+        return Bill_date;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof SupBill)) {
-            return false;
-        }
-        SupBill other = (SupBill) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setBill_date(Date Bill_date) {
+        this.Bill_date = Bill_date;
+    }
+
+    public SupPayment getSupPayment() {
+        return supPayment;
+    }
+
+    public void setSupPayment(SupPayment supPayment) {
+        this.supPayment = supPayment;
     }
 
     @Override
     public String toString() {
-        return "supsms.dao.entity.SupBill[ id=" + id + " ]";
+        return "SupBill{" + "Id_Bill=" + Id_Bill + '}';
     }
     
 }
--- Desktop/Supsms1/src/java/supsms/dao/entity/SupSubscription.java
+++ Desktop/Supsms1/src/java/supsms/dao/entity/SupSubscription.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package supsms.dao.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -18,41 +16,16 @@ import javax.persistence.Id;
 @Entity
 public class SupSubscription implements Serializable {
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof SupSubscription)) {
-            return false;
-        }
-        SupSubscription other = (SupSubscription) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "supsms.dao.entity.SupSubscription[ id=" + id + " ]";
-    }
+    @Column (name = "id_subscription")
+    private Long Id_subscription;
     
+    @Column (name = "dateSubcription", nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date DateSubcription;
+    
+    ///Id_User
+   
 }
