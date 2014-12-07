7a7ef1c59397e2e755fd902f7aa2a37cf9b63eeb
Git-zaki <AbdouBox@AbdouBox-PC>
2014-12-07 01:45:12 +0100

    One to one

--- Desktop/Supsms1/src/java/supsms/dao/entity/SupPayment.java
+++ Desktop/Supsms1/src/java/supsms/dao/entity/SupPayment.java
@@ -1,58 +1,109 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package supsms.dao.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author AbdouBox
  */
+
 @Entity
 public class SupPayment implements Serializable {
     private static final long serialVersionUID = 1L;
+   
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column (name = "id_Suply")
+    private Long Id_Suply;
+            
+    @Column (name = "firstName", nullable = false)
+    private String FirstName;
+    
+    @Column (name = "lastName", nullable = false)
+    private String LastName;
+    
+    @Column (name = "cardNumber", nullable = false)
+    private Long CardNumber;
+    
+    @Column (name = "crypto", nullable = false)
+    private Long Crypto;
+    
+    @Column (name = "expiryDate", nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date ExpiryDate;
+    
+    @OneToOne
+    @JoinColumn(name="id_Role")
+    private SupRole suprole;
 
-    public Long getId() {
-        return id;
+    public Long getId_Suply() {
+        return Id_Suply;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId_Suply(Long Id_Suply) {
+        this.Id_Suply = Id_Suply;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public String getFirstName() {
+        return FirstName;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof SupPayment)) {
-            return false;
-        }
-        SupPayment other = (SupPayment) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public Long getCardNumber() {
+        return CardNumber;
+    }
+
+    public void setCardNumber(Long CardNumber) {
+        this.CardNumber = CardNumber;
+    }
+
+    public Long getCrypto() {
+        return Crypto;
+    }
+
+    public void setCrypto(Long Crypto) {
+        this.Crypto = Crypto;
+    }
+
+    public Date getExpiryDate() {
+        return ExpiryDate;
+    }
+
+    public void setExpiryDate(Date ExpiryDate) {
+        this.ExpiryDate = ExpiryDate;
+    }
+
+    public SupRole getSuprole() {
+        return suprole;
+    }
+
+    public void setSuprole(SupRole suprole) {
+        this.suprole = suprole;
     }
 
     @Override
     public String toString() {
-        return "supsms.dao.entity.SupPayment[ id=" + id + " ]";
+        return "SupPayment{" + "Id_Suply=" + Id_Suply + '}';
     }
-    
-}
+       
+    }
